21466a3cd9ae3c6def374924370e74a51d0f3906
KHY.PHAT <khy.phat@PNC.LAN>
2022-01-25 07:47:17 +0700

    #5 fix task add question code from javascript to html

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let homePage = () =>{
 
     let container = document.createElement('div');
     container.className = 'container';
+    container.className = 'container_home';
     // 1. Logo
         // create element and class of elemen of logo
     let logo = document.createElement('div');
@@ -47,113 +48,18 @@ let homePage = () =>{
     ul.appendChild(li1);
     ul.appendChild(li2);
     container.appendChild(ul);
-    
+    console.log(container)
 }
 // homePage()
 
 
 let createQuestion = document.querySelector("#btnCreate")
-let addQuestion = () =>{
-    let container = document.createElement('div');
-    container.className = 'container';
-    document.body.appendChild(container);
+// let addQuestion = () =>{
+//     let container = document.querySelector('.create-question');
 
-    // 1. Title of the questions
-    // create title of question
-    let titleQuestion = document.createElement('div');
-    titleQuestion.className = 'create-question';
-    let title = document.createElement('div');
-    title.className = 'title';
-    let btnBack = document.createElement('button');
-    btnBack.className = 'btn-back';
-    btnBack.innerHTML = '&#x21E6;';
-    btnBack.id="btnBack"
-    let h1 = document.createElement('h1');
-    h1.className = 'title-create'
-    h1.textContent = 'CREATE QUESTION'
-    let question = document.createElement('input');
-    question.className = 'add-question';
-    question.type = 'text';
-    question.placeholder = "Add your question...";
-        // Append it to container
-    container.appendChild(titleQuestion);
-    titleQuestion.appendChild(title);
-    title.appendChild(btnBack);
-    title.appendChild(h1);
-    titleQuestion.appendChild(question);
-    // Select type of the question 
-    createChoice()
-    // Write your choice of the question
-    writeAnswers()
-    // get element to input answer and choose the correct answer
-    let choose = document.querySelector(".choice");
-    let create = document.querySelector('.create')
-    choose.addEventListener('click',changOption);
-    create.addEventListener('click', getQuestion)
-}
-let createChoice = () =>{
-    let container = document.querySelector('.container')
-    // 2. Type of the questions 
-        // create elements
-    let select = document.createElement('select');
-    select.className ="choice"
-    let option1 = document.createElement('option')
-    option1.className = 'type-of-question';
-    option1.id = 'option1';
-    option1.value = 'radio';
-    option1.textContent = 'Option Answer';
-    let option2 = document.createElement('option')
-    option2.className = 'type-of-question'
-    option2.id = 'option2'
-    option2.value = 'checkbox';
-    option2.textContent = 'Multiple Choice';
-    // To append it to the container
-    select.appendChild(option1);
-    select.appendChild(option2);
-    container.appendChild(select);
-    
-}
+//     console.log(container)
 
-// Function to write the write the answer
-let writeAnswers = () =>{
-    let container = document.querySelector('.container')
-    let form = document.createElement('form');
-    form.className = 'type-answer';
-    let typeOfQuestion = document.querySelector('.choice')
-    //3. Add form answer
-    for (i= 0; i <4; i++){
-        // create tag for input and answer
-        let inputGroup = document.createElement('div');
-        inputGroup.className ='input-group';
-        
-        // Create element input for select type radio or checkbox
-        let input_to_select = document.createElement('input');
-        let br = document.createElement("br");
-        input_to_select.type = typeOfQuestion.value;
-        input_to_select.name = 'answer';
-        input_to_select.className = 'type-of-input';
-
-        // input text that is the answer for the question
-        let input_text = document.createElement('input');
-        input_text.type = 'text';
-        input_text.className = 'answer'; 
-        input_text.placeholder = 'answer' + (i+1);
-        
-        // Append it to container
-        inputGroup.appendChild(input_to_select);
-        inputGroup.appendChild(input_text);
-        form.appendChild(inputGroup);
-        form.appendChild(br);
-        container.appendChild(form);
-    }
-    // 4. Create button create the question and append it to container
-    let btnCreate = document.createElement('button');
-    btnCreate.className = 'create';
-    btnCreate.id = 'create';
-    btnCreate.textContent = 'CREATE';
-    container.appendChild(btnCreate);
-}
-
+// }
 // Function to change the type of the question when the user want 
 function changOption(){
     let inputAnswer = document.querySelectorAll('.type-of-input');
@@ -167,7 +73,6 @@ function changOption(){
         }
     }
 }
-
 
 // Get value from the input question====================
 let getQuestion = () =>{
@@ -184,17 +89,19 @@ let getQuestion = () =>{
         }
     }
     let corrected_answer = 1
+    let choose_answer = false
     for (let i = 0; i < selects.length; i++){
         if (selects[i].checked){
             dataList['crection'+"_" + corrected_answer] = answers[i].value
+            choose_answer = true
             corrected_answer += 1
         }
     }
+
     // Alert to tell user when the user don't complete all the condition 
-    if (titleQuestion.value !== "" && answers[0].value !== "" && answers[1].value !== ""&& answers[1].value !== ""&& answers[3].value !== ""){
+    if (titleQuestion.value !== "" && answers[0].value !== "" && answers[1].value !== ""&& answers[1].value !== ""&& answers[3].value !== "" && choose_answer){
         dataList.title_question = titleQuestion.value
         data.push(dataList)
-        
         // reset the value to empty
         titleQuestion.value = ""
         for (input of answers){
@@ -206,11 +113,15 @@ let getQuestion = () =>{
         // To alert when user don't input the answers and click start
     }else if(answers[0].value == "" || answers[1].value == ""|| answers[1].value == ""|| answers[3].value == ""){
         window.alert("You have to input your answer all places!")
+    }else{
+        window.alert("Please choose the correct answer!")
     }
     console.log(data)
 }
 let data = []
-// createQuestion.addEventListener('click', addQuestion)
-addQuestion();
-
+// get element to input answer and choose the correct answer
+let choose = document.querySelector(".choice");
+let create = document.querySelector('.create')
+choose.addEventListener('click',changOption);
+create.addEventListener('click', getQuestion)
 
